95caf1f1a60c5bde8b4ad7d2d07830c8b0784d15
nssw <nssw@users.noreply.github.com>
2016-08-06 13:57:02 +0200

    Fix no-unused-vars issue
    ref commit: 99b5fcefc3bb57f2404ebd5dcd8a5d3ae064663e

--- client/state/posts/test/selectors.js
+++ client/state/posts/test/selectors.js
@@ -19,7 +19,6 @@ import {
 	isSitePostsLastPageForQuery,
 	getSitePostsForQueryIgnoringPage,
 	isRequestingSitePostsForQueryIgnoringPage,
-	isRequestingSitePost,
 	getEditedPost,
 	getPostEdits,
 	getEditedPostValue,
